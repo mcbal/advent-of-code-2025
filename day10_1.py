90ff8e07769cb922c06d0f4b14947cf90b3ca429
mcbal <matthiascbal@gmail.com>
2025-12-10 19:04:09 +0100

    Add day 10 part 1

---
+++ day10_1.py
@@ -0,0 +1,52 @@
+import sys
+from collections import deque
+from functools import cache
+
+
+def parse(lines: list[str]):
+    machines = []
+    for ln in lines:
+        lights, *buttons, joltages = ln.split()
+        machines.append(
+            {
+                "lights": tuple(c == "#" for c in list(lights[1:-1])),
+                "buttons": [tuple(map(int, b[1:-1].split(","))) for b in buttons],
+                "joltages": list(map(int, joltages[1:-1].split(","))),
+            }
+        )
+    return machines
+
+
+def fewest_button_presses(machines: list[dict]):
+    for machine_idx, machine in enumerate(machines):
+        lights, buttons = machine["lights"], machine["buttons"]
+
+        @cache
+        def toggle(state: tuple[bool], button: tuple[int]):
+            return tuple(not s if i in button else s for i, s in enumerate(state))
+
+        # bfs
+        state = tuple(False for _ in range(len(lights)))
+        queue = deque()
+        queue.extend((1, toggle(state, button), button) for button in buttons)
+        min_total_presses = 1e16
+        while queue:
+            i, state, button = queue.popleft()
+            if i > min_total_presses:
+                continue
+            if state == lights:
+                print(
+                    f"Machine {machine_idx} / {len(machines)}: {i} presses",
+                    end="\r",
+                    flush=True,
+                )
+                yield i
+                break
+            queue.extend(
+                (i + 1, toggle(state, next_button), next_button)
+                for next_button in buttons
+                if next_button != button
+            )
+
+
+print(f"\033[K{sum(fewest_button_presses(parse(sys.stdin.readlines())))}")
