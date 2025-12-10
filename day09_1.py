286fec94d7b842cbec4f2f1e079c21ff7efbcd53
mcbal <matthiascbal@gmail.com>
2025-12-10 09:27:19 +0100

    Update day 9 part 1

--- day09_1.py
+++ day09_1.py
@@ -1,26 +1,25 @@
 import sys
-import numpy as np
 
 
-def areas(coords: np.ndarray):
-    n = coords.shape[0]
-    out = np.empty(int(n * (n - 1) / 2), dtype=coords.dtype)
+def areas(coords: list[list[int, int]]):
+    n = len(coords)
+    out = [0.0] * int(n * (n - 1) / 2)
     k = 0
     for i in range(n - 1):
         for j in range(i + 1, n):
             out[k] = (
-                max(coords[i, 0], coords[j, 0]) + 1 - min(coords[i, 0], coords[j, 0])
-            ) * (max(coords[i, 1], coords[j, 1]) + 1 - min(coords[i, 1], coords[j, 1]))
+                max(coords[i][0], coords[j][0]) + 1 - min(coords[i][0], coords[j][0])
+            ) * (max(coords[i][1], coords[j][1]) + 1 - min(coords[i][1], coords[j][1]))
             k += 1
     return out
 
 
-def largest_rectangle_area(*, coords: np.ndarray):
-    return np.amax(areas(coords))
+def largest_rectangle_area(*, coords: list[list[int, int]]):
+    return max(areas(coords))
 
 
 print(
     largest_rectangle_area(
-        coords=np.asarray([list(map(int, c.split(","))) for c in sys.stdin.readlines()])
+        coords=[list(map(int, c.strip().split(","))) for c in sys.stdin.readlines()]
     )
 )
